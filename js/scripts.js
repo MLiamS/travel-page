c389d9cb17b4bfb3db1557a0f0fd2cc21c8d2469
Liam Stabeno and Shane Stafford <shane@>
2017-02-27 11:58:53 -0800

    update travel page to include proper html, css tags and add locale, landmark and season attributes to place constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
-// Back-End
-
-//Front-End
-$(function)(){
-
-
+//Business logic
+function Place(locale, landmark, season) {
+  this.locale = locale;
+  this.landmark = landmark;
+  this.season = season;
 }
+Place.prototype.previewPlace = function() {
+  return this.locale;
+}
+//User interface logic
+$(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedLocale = $("input#new-locale").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedSeason = $("input#new-season").val();
+
+    var newPlace = new Place(inputtedLocale, inputtedLandmark, inputtedSeason);
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.previewPlace() + "</span></li>");
+
+    $(".place").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.locale);
+      $(".location").text(newPlace.locale);
+      $(".landmark").text(newPlace.landmark);
+      $(".season").text(newPlace.season);
+    });
+
+  });
+});
